4fc54ec9e0a76b97dde0c5d7d45bf5f7c6afbabf
Renat Nurgaliyev <renat@rnu.one>
2024-02-06 11:10:18 +0100

    Minor updates
    * Tested compatibility with DSM7.2.1-69057 Update 4
    * Bump alpine version
    * Prefer more popular docker over podman

--- run-container.sh
+++ run-container.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-if podman -v >/dev/null 2>&1; then 
-    CM=podman
-elif docker -v >/dev/null 2>&1; then
+if docker -v >/dev/null 2>&1; then 
     CM=docker
+elif podman -v >/dev/null 2>&1; then
+    CM=podman
 else
-    echo "Container manager not installed. Please install podman or docker."
+    echo "Container manager not installed. Please install docker or podman."
     exit 1
 fi
 
@@ -21,7 +21,6 @@ fi
 
 echo "=== Starting new container"
 $CM run -d --name dsm-kmip-server -p 5696:5696 \
-    --restart=unless-stopped \
     --mount type=bind,source="$WORKDIR"/state,target=/var/lib/state \
     --mount type=bind,source="$WORKDIR"/certs,target=/var/lib/certs \
     dsm-kmip-server:latest
